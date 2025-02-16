8570b0323652c0c26867ee3ac2527957e966fb87
とだ <identity3823@gmail.com>
2025-02-16 17:50:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -117,14 +117,14 @@ def generate_wordcloud():
             text = " ".join(words)
 
         # マスク画像の読み込み（try ブロック内に正しくインデントする）
-        mask = np.array(Image.open("./picture/n.png"))
+        mask = np.array(Image.open("./picture/human.png"))
 
         # ワードクラウド生成（日本語フォントを指定）
         wc = WordCloud(
             font_path="./fonts/NotoSansJP-Medium.ttf",
             background_color="#fff8e8",
-            width=1000,
-            height=1000,
+            width=2000,
+            height=2000,
             max_words=100,
             mask=mask,
         ).generate(text)
